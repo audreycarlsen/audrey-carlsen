8f47039e401308c6e41110be1671ef80289677ec
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-16 21:04:39 -0700

    create post works

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Audreycarlsen::Application.routes.draw do
   get   '/digital',       to: 'welcome#digital', as: 'digital'
   get   '/radio',         to: 'welcome#radio',   as: 'radio'
   get   '/blog',          to: 'posts#index',     as: 'blog'
-  get   '/blog/new',      to: 'posts#new',       as: 'new_post'
-  post  '/blog',          to: 'posts#create'
+  get   '/blog/new',      to: 'posts#new',       as: 'posts'
+  post  '/blog/new',      to: 'posts#create'
   get   '/blog/:id',      to: 'posts#show',      as: 'post'
   get   '/blog/:id/edit', to: 'posts#edit',      as: 'edit_post'
   patch '/blog/:id',      to: 'posts#update'
   get   '/about',         to: 'welcome#about',   as: 'about'
 
+  get   '/admin',         to: 'admin#index'
   # resources :posts
 end
