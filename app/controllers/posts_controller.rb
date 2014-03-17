1a52c904d4b4b1fc3a725ac9572c750d413b8253
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-16 20:56:00 -0700

    update is working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,37 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.all.order('created_at DESC')
-    @posts = Post.paginate(:page => params[:page], :per_page => 1)
+    @posts = Post.paginate(:page => params[:page], :per_page => 1).order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to blog_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Audreycarlsen::Application.routes.draw do
   root 'welcome#index'
 
-  get '/',         to: 'welcome#index',   as: 'index', layout: false
-  get '/digital',  to: 'welcome#digital', as: 'digital'
-  get '/radio',    to: 'welcome#radio',   as: 'radio'
-  get '/blog',     to: 'posts#index',     as: 'blog'
-  get '/about',    to: 'welcome#about',   as: 'about'
+  get   '/',              to: 'welcome#index',   as: 'index', layout: false
+  get   '/digital',       to: 'welcome#digital', as: 'digital'
+  get   '/radio',         to: 'welcome#radio',   as: 'radio'
+  get   '/blog',          to: 'posts#index',     as: 'blog'
+  get   '/blog/new',      to: 'posts#new',       as: 'new_post'
+  post  '/blog',          to: 'posts#create'
+  get   '/blog/:id',      to: 'posts#show',      as: 'post'
+  get   '/blog/:id/edit', to: 'posts#edit',      as: 'edit_post'
+  patch '/blog/:id',      to: 'posts#update'
+  get   '/about',         to: 'welcome#about',   as: 'about'
+
+  # resources :posts
 end
