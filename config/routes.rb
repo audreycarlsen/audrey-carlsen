9b3193d225e44778531020470e11480e7ca8b2d4
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-19 10:34:58 -0700

    galina saves the world

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Audreycarlsen::Application.routes.draw do
   root 'welcome#index'
 
-  get    '/',                 to: 'welcome#index',   as: 'index', layout: false
+  get    '/',              to: 'welcome#index',   as: 'index', layout: false
   get    '/digital',       to: 'welcome#digital', as: 'digital'
   get    '/radio',         to: 'welcome#radio',   as: 'radio'
   get    '/blog',          to: 'posts#index',     as: 'blog'
   get    '/blog/new',      to: 'posts#new',       as: 'posts'
   post   '/blog/new',      to: 'posts#create'
+  delete '/blog/:id',      to: 'posts#destroy',   as: 'delete_post'
   get    '/blog/:id',      to: 'posts#show',      as: 'post'
   get    '/blog/:id/edit', to: 'posts#edit',      as: 'edit_post'
   patch  '/blog/:id',      to: 'posts#update'
-  delete '/blog/:id',      to: 'posts#destroy',   as: 'delete_post'
   get    '/about',         to: 'welcome#about',   as: 'about'
 
   get    '/log_in',        to: 'session#new',     as: 'log_in'
